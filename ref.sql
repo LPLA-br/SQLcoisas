793a1d335e7696b78b0ec13b35d3340582890c32
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2022-12-07 15:52:51 -0300

    commit 6

--- ref.sql
+++ ref.sql
@@ -1,30 +1,54 @@
 -- Este código é uma referência Não executável.
--- LPLA-br 2022 - All rights reserved
+-- LPLA-br 2022 --
+
+--NOTA: Toda tabela que referência outra(s) devem ser
+--	definidas por último(código lido de cima para baixo).
 
 ------------------- SEÇÃO NULL -------------------
 
---Operadores Aritméticos de soma,subração,
---multiplicação,divisão e módulo = javascript
+--OPERADORES MATEMÁTICOS
+--	+		Soma
+--	-		Subtração
+--	*		Múltiplicação
+--	/	DIV	Divisão
+--	%	MOD	Módulo
 
---WHERE condição_lógica;
---	=	igual a (comparação)
---	<>	não igual a (comparação)
---	>	maior que
---	<	menor que
---	>=	maior que ou igual
---	<=	menor que ou igual
+--OPERADORES DE ATRIBUIÇÃO
+--	:=	atribuir valor
 
---MYSQL
+--OPERADORES DE COMPARAÇÃO
+--	=		     	igual a
+--	<>		     	não igual a 
+--	>		     	maior que
+--	<		     	menor que
+--	>=		     	maior que ou igual
+--	<=		     	menor que ou igual
+--	<=>			NULL-safe equal Operador
+--	BETWEEN x AND y		TRUE se entre x e y (pode ser negado)
+-- 	COALESCE		Retorna o primeiro valor não NULL
+--	GREATEST		Retorna o maior argumento
+--	LEAST			Retorna o menor argumento
+--	IS			é (pode ser negado)
+--	IS NULL			é núlo ?
+--				em ...
+
+-- OPERADORES LÓGICOS
+--	!	NOT
+--	&&	AND
+--		XOR
+--	||	OR
+
+-- ?
 --	ALL
---	AND	operador lógico E
 --	ANY
---	BETWEEN
 --	EXISTS	existe(pode ter prefixo de negação:NOT IN)
 --	IN	está em(pode ter prefixo de negação:NOT IN)
---	LIKE	expressões regulares SQL?
---	NOT	operador lógico de NEGAÇÃO.
---	OR	operador lógico de OU
+--	LIKE	expressões regulares estilo sql
 --	SOME
+
+--	ORDER BY atributo [ASC|DESC]
+
+--	GROUP BY atributo [ASC|DESC][HAVING] logica;
 
 --CONSTRAINTS (restrições)
 --	NOT NULL	valor obrigatório
@@ -38,7 +62,9 @@
 --DOMINIOS
 --	CHAR(tamanhofixo)
 --	VARCHAR(tamanho)
+--	TEXT
 --	INT ou INTERGER
+--	INT UNSIGNED
 --	SMALLINT
 --	NUMERIC(parteInteira,parteDecimal)
 --	REAL, DOUBLE PRECISION
@@ -48,10 +74,19 @@
 --	% fuciona como * do globbing do shell bash
 --	_ fuciona como ? do globbing do shell bash
 
-------------------- SEÇÃO I -------------------
--- DOS BANCOS DE DADOS E TABELAS --
+--FUNÇÕES UTEIS
+-- now() retorna string de data atual.
 
--------------------------------------------CREATE DATABASE
+--FUNÇÕES DE AGREGAÇÃO
+--avg()		média Aritmética
+--min()		menor valor
+--max()		maior valor
+--sum()		somatória
+--count()	contagem
+
+------------------- SEÇÃO I --------------------- DOS BANCOS DE DADOS E TABELAS --
+
+-------------------------------------------CREATE DATABASE----------------------------------------
 
 -- Criar banco de dados: CREATE DATABASE
 CREATE DATABASE IF NOT EXISTS Supermercado;
@@ -60,9 +95,9 @@ CREATE SCHEMA IF NOT EXISTS Example;
 -- USE define seu database de trabalho atual.
 USE Supermercado;
 
--- A notação "nomeBancoDados.nomeTabela" funciona.
+--Se você não utilizar o USE terá que referenciar na forma BANCODEDADOS.TABELA
 
--------------------------------------------CREATE TABLE
+-------------------------------------------CREATE TABLE----------------------------------------
 
 CREATE TABLE Fornecedores (
 	Cod_Fornec SMALLINT PRIMARY KEY AUTO_INCREMENT,
@@ -84,14 +119,14 @@ CREATE TABLE Produtos (
 -- Mostrar Tabelas de um database sem USE.
 SHOW TABLES FROM Supermercado;
 
--- Descreve Tabelas de um database
+-- Descreve Tabelas de um database (com USE).
 DESCRIBE Produtos;
 DESCRIBE Forncenecedores;
 
--- Criará Produtos se ele não existe ainda.
+-- Criará a tabela Produtos somente se ela não existe ainda.
 CREATE TABLE IF NOT EXISTS Produtos;
 
--------------------------------------------INSERT INTO
+-------------------------------------------INSERT INTO----------------------------------------
 
 INSERT INTO Fornecedores (Cod_Fornec, Nome_Fornec)
 VALUES (1, "Redstene"), (2, "EQR");
@@ -104,13 +139,14 @@ VALUES (100, "Monitor LCD", 12, 550.00, 1, "ak9ig"),
 (104, "Webcam", 6, 170.00, 2, "morl1"),
 (105, "Marreta Mágica", 2, 9756.50, 2, "de6se");
 
-------------------------------------------ALTER TABLE
+------------------------------------------ALTER TABLE----------------------------------------
 
 -- Adicionar Coluna em uma tabela.
+-- COLUMN opcional(depende das versões do mysql)
 
 ALTER TABLE Produtos
-ADD Volume VARCHAR(10),
-ADD Peso SMALLINT NOT NULL;
+ADD COLUMN Volume VARCHAR(10),
+ADD COLUMN Peso SMALLINT NOT NULL;
 
 -- Modificar definição da coluna.
 
@@ -126,25 +162,26 @@ ALTER TABLE Produtos
 DROP COLUMN Volume,
 DROP COLUMN Peso;
 
---------------------------------------------SELECT FROM
+--------------------------------------------SELECT FROM----------------------------------------
 
 --CONSULTAS
 
 -- Efetuar consulta (query), trazendo colunas específicas.
 
-SELECT Nome_Fornec FROM Fornecedores; 		--uma coluna especificas.
-SELECT Nome_Prod, Preco_Prod FROM Produtos; 	--2 colunas especificas.
 SELECT * FROM Produtos; 			--toda a tabela.
+SELECT Nome_Fornec FROM Fornecedores; 		--uma coluna especifica.
+SELECT Nome_Prod, Preco_Prod FROM Produtos; 	--duas colunas especificas.
 SELECT DISTINCT Nome_Prod FROM Produtos; 	--retorna coluna omitindo valores iguais.
 
+--EXPRESSÕES REGULARES ESTILO SQL
 --retorna todos produtos comessados por M.
-SELECT Nome_Prod FROM Produtos WHERE Nome_Prod like "M%"; 
+SELECT Nome_Prod FROM Produtos WHERE Nome_Prod LIKE "M%"; 
 --retorna todos os produtos com 4 caracteres de tamanho no nome.
-SELECT Nome_Prod FROM Produtos WHERE Nome_Prod like "____";
+SELECT Nome_Prod FROM Produtos WHERE Nome_Prod LIKE "____";
 --retorna todos os produtos com pelomenos 4 caracteres de tamanho no nome.
-SELECT Nome_Prod FROM Produtos WHERE Nome_Prod like "____%";
+SELECT Nome_Prod FROM Produtos WHERE Nome_Prod LIKE "____%";
 --retorna todos os produtos que contém Marreta no nome.
-SELECT Nome_Prod FROM Produtos WHERE Nome_Prod like "%Marreta%";
+SELECT Nome_Prod FROM Produtos WHERE Nome_Prod LIKE "%Marreta%";
 
 -- Consutando uma tabela com filtro de dados específicos E lógica.
 -- após WHERE tu podes especificar expressões lógicas complexas.
@@ -169,44 +206,20 @@ WHERE ID_Prod >= 101 AND ( Quant_Prod >= 1  OR Preco_Prod > 50.0  );
 --         2   ana      200      a
 --         3   clinton	400      b
 --         4   marco    405      c
+--         5   júlia	543	 a
+--         6   ania	734	 c
 
 -- considere IN como "está em..."
 SELECT nome FROM Cliente WHERE bairro IN ("a", "b");
--- → josé ana clinton
+-- → josé ana clinton júlia
 SELECT nome FROM Cliente WHERE bairro NOT IN ("a","b");
--- → marco
+-- → marco ania
 
--- ordenação ascedente e descedente - alfabéticamente ou numericamente.
-SELECT nome, credito FROM Cliente ORDER BY nome DESC
-SELECT nome, credito FROM Cliente ORDER BY nome ASC
-
--------------------------------------------UPDATE
-
--- Alterar um registro na tabela: UPDATE
-
-UPDATE Produtos
-SET Preco_Prod = 999.00, Quant_Prod = 15
-WHERE ID_Prod = 100;
-
---------------------------------------------DELETE
-
--- Excluir tupla de uma tabela: DELETE FROM
-
-DELETE FROM Produtos
-WHERE ID_Prod = 101; --Omissão de WHERE resulta em deleção de tudo.
-
----------------------------------------------INNER JOIN
-
--- Trazer dados de duas ou mais tabelas relacionadas: INNER JOIN
--- INNER JOIN selects records that have matching values in both
--- tables.
-
-SELECT Nome_Prod, Nome_Fornec
-FROM Fornecedores
-INNER JOIN Produtos
-ON Fornecedores.Cod_Fornec = Produtos.Cod_Fornec;
-
--- Retornar resultados ordenados em uma consulta
+--ORDER BY 
+--Retornar resultados ordenados em uma consulta
+-- ordenação ascedente e descedente - alfabeticamente ou numericamente.
+SELECT nome, credito FROM Cliente ORDER BY nome DESC;
+SELECT nome, credito FROM Cliente ORDER BY nome ASC;
 
 SELECT Nome_Prod, Preco_Prod
 FROM Produtos
@@ -220,14 +233,55 @@ SELECT Nome_Prod
 FROM Produtos
 ORDER BY Nome_Prod DESC; --ordem alfabética descedente.
 
------------------------------------------------TRUNCATE
+--ALIAS e GROUP BY
+-- Pega todas as iguais ocorrências e joga em potes acomuladores.
+SELECT bairro AS bandeclay FROM Cliente GROUP BY bandeclay ASC;
+SELECT bairro AS bandeclay FROM Cliente GROUP BY bandeclay DESC;
+
+-------------------------------------------------------------------
+-- TESTADOS NO MARIADB com o banco de dados demostrativo do mysql:-
+-- world com tabelas: city, country, countrylanguage		  -
+-------------------------------------------------------------------
+
+--retorna o número de cidades por 'distrito'
+-- HAVING
+SELECT CountryCode, District, count(Name) AS NumeroDeCidadesPorDistrito
+FROM city GROUP BY District HAVING CountryCode = 'BRA' ORDER BY NumeroDeCidadesPorDistrito DESC;
+
+--faz o mesmo do de cima só que aversamente :-)
+SELECT CountryCode, District, count(Name) AS NumeroDeCidadesPorDistrito
+FROM city GROUP BY District HAVING NOT CountryCode <> 'BRA' ORDER BY NumeroDeCidadesPorDistrito DESC;
+
+--línguas e número de paises em que é falada.
+SELECT Language, count(CountyCode) FROM countrylanguage GROUP BY Language;
+--português
+SELECT Language, count(CountyCode) FROM countrylanguage GROUP BY Language HAVING Language = "portuguese";
+--paises com mais de 50.0 porcento de falantes de português.
+
+
+-------------------------------------------UPDATE----------------------------------------
+
+-- Alterar um registro na tabela: UPDATE
+
+UPDATE Produtos
+SET Preco_Prod = 999.00, Quant_Prod = 15
+WHERE ID_Prod = 100;
+
+--------------------------------------------DELETE----------------------------------------
+
+-- Excluir tupla de uma tabela: DELETE FROM
+-- Operações de UPDATE e DELETE são afetadas pela relação: primary key e foreign key
+
+DELETE FROM Produtos
+WHERE ID_Prod = 101; --DELETE sem WHERE resulta em deleção de tudo.
+
+-----------------------------------------------TRUNCATE----------------------------------------
 
 -- Apagar todos os dados de uma tabela. Não toda a tabela.
 
-TRUNCATE TABLE Produtos
-SELECT * FROM Produtos; --tabela produtos não mais existe.
+TRUNCATE TABLE Produtos;
 
------------------------------------------------DROP
+-----------------------------------------------DROP----------------------------------------
 
 -- Deletar a tabela inteira.
 
@@ -237,11 +291,11 @@ DROP TABLE IF EXISTS Produtos;
 
 DROP DATABASE IF EXISTS Supermercado;
 
-------------------- SEÇÃO II -------------------
--- DOS USUÁRIOS E CONTAS --
+------------------- SEÇÃO II -------------------------- DOS USUÁRIOS E CONTAS --
+
 -- apenas root ou um usuário poderoso
 -- do SGBD pode operar aqui.
--- localhost = 127.0.0.1 --ou Não
+-- localhost = 127.0.0.1
 
 CREATE USER IF NOT EXISTS "Marcolino"@"localhost" IDENTIFIED BY "senhaForte123";
 
@@ -263,4 +317,17 @@ REVOKE INSERT ON Tabela TO "Marcolino"@"loclhost";
 SHOW GRANTS FOR "Marcolino"@"localhost";
 
 
+------------------- SEÇÃO UNIX --------------------- DA LINHA DE COMANDO LINUX --
+
+--  Iniciar sessão interativa:
+--	mysql -u USUARIO -p		u=usuario; p=senha;
+
+--  Executar script sql
+--	mysql -u USUARIO -p < NOME.sql
+
+--  Executar e imprimir no terminal
+--	mysql -u USUARIO -t -e "INSTRUÇÃO_SQL;" -p	t=formatoDeTabela e=executar
+
+--  Backup(cria arquivo .sql no diretório corrente com o backup)
+--	mysqldump -u USUARIO -p BANCODADOSNOME > NOME.sql
 
